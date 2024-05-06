4fe95404fbb87023374af424cd35ebe4bcce747b
sofa-org <tech@sofa.org>
2024-05-06 20:31:15 +0800

    align variable name

--- contracts/strategies/SmartBull.sol
+++ contracts/strategies/SmartBull.sol
@@ -7,13 +7,13 @@ import "../interfaces/ISmartTrendStrategy.sol";
 
 
 contract SmartBull is ISmartTrendStrategy {
-    function getMakerPayoff(uint256[2] memory anchorPrices, uint256 settlePrice, uint256 maxPayoff) public pure returns (uint256 realizedPayoff) {
+    function getMakerPayoff(uint256[2] memory anchorPrices, uint256 settlePrice, uint256 maxPayoff) public pure returns (uint256 payoff) {
         if (settlePrice >= anchorPrices[1])
-            realizedPayoff = 0;
+            payoff = 0;
         else if (settlePrice <= anchorPrices[0])
-            realizedPayoff = maxPayoff;
+            payoff = maxPayoff;
         else
-            realizedPayoff = maxPayoff * (anchorPrices[1] - settlePrice) / (anchorPrices[1] - anchorPrices[0]);
+            payoff = maxPayoff * (anchorPrices[1] - settlePrice) / (anchorPrices[1] - anchorPrices[0]);
     }
 
     function getMinterPayoff(uint256[2] memory anchorPrices, uint256 settlePrice, uint256 maxPayoff) external pure returns (uint256) {
