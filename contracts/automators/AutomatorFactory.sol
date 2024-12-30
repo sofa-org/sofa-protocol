73ea5c5dd594ecd5f38bb016086938bb9d1eeda7
sofa-org <tech@sofa.org>
2024-12-30 17:28:34 +0800

    refactor the directory

--- contracts/automators/AutomatorFactory.sol
+++ contracts/automators/AutomatorFactory.sol
@@ -4,8 +4,7 @@ pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./AAVEAutomatorBase.sol";
-import "hardhat/console.sol";
+import "./bases/AAVEAutomatorBase.sol";
 
 contract AutomatorFactory is Ownable {
     address public referral;
